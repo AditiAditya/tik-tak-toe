05d0d1042633a95b8bb6eacc266c9162e3352eaf
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-05-03 20:08:17 +0530

    2commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-VarDate
+console.log("welcome to Tic Tak Toe")
+let audioturn=  new Audio ("new.wav")
+let gameover= new Audio("gameover.mp3 U ")
+let turn = "X";
+
+const changeTurn= ()=>{
+return  turn=== "X"? " 0" : X
+
+}
+
+const checkwin =()=>{
+
+}
+
+let  boxes   =  document .getElementByClassName("box");
+Array.from(boxes).forEach(elements => {
+    let boxtext= element.querySelector(".boxtex");
+      element.addEventListener('click', ()=>{
+      if (boxtext. innerTex=== ' ' ){
+        boxtext.innerText = turn;
+        changeTurn ();
+         audioturn.play();
+         checkWin();
+         document.getElementsByClassName("info")[0].innerText  = ' TURN FOR' + turn
+      }
+      
+      })
+})
+
+
