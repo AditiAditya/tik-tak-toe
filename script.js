993f32d45a947de66021804ae251342cff026291
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-05-10 15:47:15 +0530

    4commir

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let m = Math;
-console.log(m);
+console.log("welcome to Tic Tak Toe")
+let audioturn=  new Audio ("new.wav")
+let gameover= new Audio("gameover.mp3 U ")
+let turn = "X";
 
-console.log("5.8 round down to the nearest integer " , Math.ceil(5.8))
+const changeTurn= ()=>{
+return  turn=== "X"? " 0" : X
 
+}
 
+const checkwin =()=>{
 
+}
+
+let  boxes   =  document .getElementByClassName("box");
+Array.from(boxes).forEach(elements => {
+    let boxtext= element.querySelector(".boxtex");
+      element.addEventListener('click', ()=>{
+      if (boxtext. innerTex=== ' ' ){
+        boxtext.innerText = turn;
+        changeTurn ();
+         audioturn.play();
+         checkWin();
+         document.getElementsByClassName("info")[0].innerText  = ' TURN FOR' + turn
+      }
+
+      })
+})
