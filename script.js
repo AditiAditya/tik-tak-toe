bdbe81beaea7784b137babfa95e372a958a80633
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-05-12 00:11:31 +0530

    5commit

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-console.log("welcome to Tic Tak Toe")
-let audioturn=  new Audio ("new.wav")
-let gameover= new Audio("gameover.mp3 U ")
-let turn = "X";
+// console.log("welcome to Tic Tak Toe")
+// let audioturn=  new Audio ("new.wav")
+// let gameover= new Audio("gameover.mp3 U ")
+// let turn = "X";
 
-const changeTurn= ()=>{
-return  turn=== "X"? " 0" : X
+// const changeTurn= ()=>{
+// return  turn=== "X"? " 0" : X
 
+// }
+
+// const checkwin =()=>{
+
+// }
+
+// let  boxes   =  document .getElementByClassName("box");
+// Array.from(boxes).forEach(elements => {
+//     let boxtext= element.querySelector(".boxtex");
+//       element.addEventListener('click', ()=>{
+//       if (boxtext. innerTex=== ' ' ){
+//         boxtext.innerText = turn;
+//         changeTurn ();
+//          audioturn.play();
+//          checkWin();
+//          document.getElementsByClassName("info")[0].innerText  = ' TURN FOR' + turn
+//       }
+
+//       })
+// })
+
+///////practice\\\\\\\\\\\\\\\\\\\\\\
+
+// function my(see){
+// console.log(see)
+// }
+
+// my("aditi");
+
+class hello{
+          message() {
+            console.log("hwllo everyone")
+          }
+sorry(){
+console.log("sorry everyoenggugug")
+}
 }
 
-const checkwin =()=>{
+let a = new hello();
+a.message()
 
-}
-
-let  boxes   =  document .getElementByClassName("box");
-Array.from(boxes).forEach(elements => {
-    let boxtext= element.querySelector(".boxtex");
-      element.addEventListener('click', ()=>{
-      if (boxtext. innerTex=== ' ' ){
-        boxtext.innerText = turn;
-        changeTurn ();
-         audioturn.play();
-         checkWin();
-         document.getElementsByClassName("info")[0].innerText  = ' TURN FOR' + turn
-      }
-
-      })
-})
+a.sorry()
