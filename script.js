47f6ef915148728c7915b46094da4ab6ed44b4a8
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-05-20 15:46:24 +0530

    leave 20 05

--- script.js
+++ script.js
@@ -1,123 +1,28 @@
-// console.log("welcome to Tic Tak Toe")
-// let audioturn=  new Audio ("new.wav")
-// let gameover= new Audio("gameover.mp3 U ")
-// let turn = "X";
+console.log("welcome to Tic Tak Toe")
+let audioturn=  new Audio ("new.wav")
+let gameover= new Audio("gameover.mp3 U ")
+let turn = "X";
 
-// const changeTurn= ()=>{
-// return  turn=== "X"? " 0" : X
+const changeTurn= ()=>{
+return  turn=== "X"? " 0" : X
 
-// }
+}
 
-// const checkwin =()=>{
+const checkwin =()=>{
 
-// }
+}
 
-// let  boxes   =  document .getElementByClassName("box");
-// Array.from(boxes).forEach(elements => {
-//     let boxtext= element.querySelector(".boxtex");
-//       element.addEventListener('click', ()=>{
-//       if (boxtext. innerTex=== ' ' ){
-//         boxtext.innerText = turn;
-//         changeTurn ();
-//          audioturn.play();
-//          checkWin();
-//          document.getElementsByClassName("info")[0].innerText  = ' TURN FOR' + turn
-//       }
+let  boxes   =  document .getElementByClassName("box");
+Array.from(boxes).forEach(elements => {
+    let boxtext= element.querySelector(".boxtex");
+      element.addEventListener('click', ()=>{
+      if (boxtext. innerTex=== ' ' ){
+        boxtext.innerText = turn;
+        changeTurn ();
+         audioturn.play();
+         checkWin();
+         document.getElementsByClassName("info")[0].innerText  = ' TURN FOR' + turn
+      }
 
-//       })
-// })
-
-///////practice\\\\\\\\\\\\\\\\\\\\\\
-
-// function my(see){
-// console.log(see)
-// }
-
-// my("aditi");
-
-// class hello{
-//           message() {
-//             console.log("hwllo everyone")
-//           }
-// sorry(){
-// console.log("sorry everyoenggugug")
-// }
-// }
-
-// let a = new hello();
-// a.message()
-
-// a.sorry()
-
-
-// let name = ["adit", "adj", "aes", "asxz"]
-// for(let i=0; i<name.length; i++){
-//   console.log(name[i])
-// }
-
-// let cities=['bhopal',"delhi","mumbai","kolkata"];
-
-// for(let city of cities){
-// console.log(city.toUpperCase())
-// }
-
-// let sum= [85, 97, 44, 37, 76, 60];
-// let marks=0
-
-// for(let no of sum){
-//   marks+=no
-
-
-// }
-// let avg= marks/sum.length
-
-// console.log(`avrg marks of student is  = ${avg}`)
-
-
-// let  items = [250, 645 , 300 , 900 , 50]
-
-// let idx = 0;
-
-// for(let val of items ){
-// console.log(`the value of ${idx} = ${items}`)
-
-// }
-
-
-// class hey{
-//           message(){
-//             console.log("Hello eveeryone ,,its aditi ");
-//           }
-// call(){
-//     console.log("i am expert in coding >.....")
-// }
-
-// hello(){
-//     console.log("hii \"its expert\" ")
-// }
-
-// }
-
-// let a = new hey ();
-// a.message()
-// let s = new hey
-// s.call()
-
-// let d = new hey
-// d.hello()
-////////////////////(//////////      19 05 2024     /////////////////////////////////
-
-// class worker{
-// constructor(){
-//   console.log(" ")
-// }
-// }
-// let z = {
-//   name: "aditi",
-//   language: "javascript"
-// }
-// console.log(z)  
-
-alert( "enter  ")
-let a = prompt ("say  something")
-document.write(a)
+      })
+})
